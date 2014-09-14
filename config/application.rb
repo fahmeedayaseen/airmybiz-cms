e7c65ecf2dc1cf01a704f8b5f6c119b8e9442f48
RAC <ammar@V570c.(none)>
2014-09-14 05:10:29 +0500

    app.rb changed.

--- config/application.rb
+++ config/application.rb
@@ -24,23 +24,23 @@ module AirmybizCms
 
     config.filter_parameters += [:password]
 
-    config.assets.enabled = true
+    #config.assets.enabled = true
 
     config.assets.version = '1.0'
 
-    initializer 'setup_asset_pipeline', :group => :all  do |app|
-      # We don't want the default of everything that isn't js or css, because it pulls too many things in
-      app.config.assets.precompile.shift
-
-      # Explicitly register the extensions we are interested in compiling
-      app.config.assets.precompile.push(Proc.new do |path|
-        File.extname(path).in? [
-                                   '.html', '.erb', '.haml',                 # Templates
-                                   '.png',  '.gif', '.jpg', '.jpeg',         # Images
-                                   '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-                               ]
-      end)
-    end
+    #initializer 'setup_asset_pipeline', :group => :all  do |app|
+    #  # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    #  app.config.assets.precompile.shift
+    #
+    #  # Explicitly register the extensions we are interested in compiling
+    #  app.config.assets.precompile.push(Proc.new do |path|
+    #    File.extname(path).in? [
+    #                               '.html', '.erb', '.haml',                 # Templates
+    #                               '.png',  '.gif', '.jpg', '.jpeg',         # Images
+    #                               '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+    #                           ]
+    #  end)
+    #end
 
   end
 end
